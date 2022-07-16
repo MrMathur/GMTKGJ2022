f08b2435b865ceba03a170999b66bec93fc790cb
Arpit Mathur <arpitmathur19@gmail.com>
2022-07-16 03:06:26 -0400

    Printing Jugaad

--- Assets/Scripts/tempNumberScript.cs
+++ Assets/Scripts/tempNumberScript.cs
@@ -10,10 +10,18 @@ public class tempNumberScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            GameObject newFace = Instantiate(faces[0], bottomFace.transform.position, bottomFace.transform.rotation);
+        void PrintDot(int index) {
+            GameObject newFace = Instantiate(faces[index], bottomFace.transform.position, bottomFace.transform.rotation);
             newFace.transform.parent = transform;
-            newFace.transform.localScale = bottomFace.transform.localScale;
+            newFace.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))  PrintDot(1);
+        if (Input.GetKeyDown(KeyCode.Alpha2))  PrintDot(2);
+        if (Input.GetKeyDown(KeyCode.Alpha3))  PrintDot(3);
+        if (Input.GetKeyDown(KeyCode.Alpha4))  PrintDot(4);
+        if (Input.GetKeyDown(KeyCode.Alpha5))  PrintDot(5);
+        if (Input.GetKeyDown(KeyCode.Alpha6))  PrintDot(6);
     }
 }
