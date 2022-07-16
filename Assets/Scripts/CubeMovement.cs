32b4f57bc31bca9e1fd11e761e5554789a66f1f5
Arpit Mathur <arpitmathur19@gmail.com>
2022-07-16 05:19:28 -0400

    Jumping from Spring

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -10,35 +10,34 @@ public class CubeMovement : MonoBehaviour
     [SerializeField] private GameObject bottomFace;
     private bool _isMoving;
 
+    private Vector3 lastMove;
+
     void Update()
     {
 
         if (_isMoving) return;
 
         void InitiateRoll(Vector3 dir) {
+            lastMove = dir;
             Vector3 anchor = transform.position + (Vector3.down + dir) * 5f;
             Vector3 axis = Vector3.Cross(Vector3.up, dir);
 
             StartCoroutine(Roll(anchor, axis));
         }
 
-        void InitiateJump(Vector3 dir) {
-            Vector3 anchor = transform.position + (dir * 10f);
-            Vector3 axis = Vector3.Cross(Vector3.up, dir);
-
-            StartCoroutine(Jump(anchor, axis));
-        }
-
-
         if (Input.GetKeyDown(KeyCode.A)) InitiateRoll(Vector3.left);
         if (Input.GetKeyDown(KeyCode.D)) InitiateRoll(Vector3.right);
         if (Input.GetKeyDown(KeyCode.W)) InitiateRoll(Vector3.forward);
         if (Input.GetKeyDown(KeyCode.S)) InitiateRoll(Vector3.back);
+    }
 
-        if (Input.GetKeyDown(KeyCode.J)) InitiateJump(Vector3.left);
-        if (Input.GetKeyDown(KeyCode.L)) InitiateJump(Vector3.right);
-        if (Input.GetKeyDown(KeyCode.I)) InitiateJump(Vector3.forward);
-        if (Input.GetKeyDown(KeyCode.K)) InitiateJump(Vector3.back);
+
+
+    private void InitiateJump(Vector3 dir) {
+        Vector3 anchor = transform.position + (dir * 10f);
+        Vector3 axis = Vector3.Cross(Vector3.up, dir);
+
+        StartCoroutine(Jump(anchor, axis));
     }
 
     IEnumerator Roll(Vector3 anchor, Vector3 axis) {
@@ -64,5 +63,9 @@ public class CubeMovement : MonoBehaviour
         }
 
         _isMoving = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Spring") InitiateJump(lastMove);
     }
 }
