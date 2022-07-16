1c6863213991c587ef87ee7e4eb7b04af5914462
Arpit Mathur <arpitmathur19@gmail.com>
2022-07-16 04:49:46 -0400

    glitchy printing mechanic

--- Assets/Scripts/PasteNumberToBottom.cs
+++ Assets/Scripts/PasteNumberToBottom.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class PasteNumberToBottom : MonoBehaviour
 {
-    
+
+    [SerializeField] private int faceValue = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<tempNumberScript>().PrintDot(faceValue);
+    }
 }
