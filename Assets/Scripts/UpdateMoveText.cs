8608816b4d4bc4b65fea1b7e56176a719d2509e3
Guzzler <sharangpai123@gmail.com>
2022-07-17 18:54:30 +0530

    HUD medal push

--- Assets/Scripts/UpdateMoveText.cs
+++ Assets/Scripts/UpdateMoveText.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class UpdateMoveText : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text moveText;
+    [SerializeField] private TMP_Text silverMoves;
+    [SerializeField] private TMP_Text goldMoves;
+
+
     private GameObject environment;
+    private GameObject medalImage;
+    public GameObject currentMedal;
     [SerializeField] private bool fullText = false;
+
+    public Sprite bronze;
+    public Sprite silver;
+    public Sprite gold;
+
 
     void Start() {
         environment = GameObject.FindGameObjectWithTag("environment");
+        medalImage = GameObject.FindGameObjectWithTag("medal_level_tag");
+        goldMoves.SetText(environment.GetComponent<Environment>().moves_gold.ToString());
+        silverMoves.SetText(environment.GetComponent<Environment>().moves_silver.ToString());
     }
     
 
@@ -23,6 +39,22 @@ public class UpdateMoveText : MonoBehaviour
             moveText.SetText("" + environment.GetComponent<Environment>().moveCounter);
         } else {
             moveText.SetText(environment.GetComponent<Environment>().moveCounter + " Moves");
+        }
+        if(PlayerStats.Levels[SceneManager.GetActiveScene().buildIndex - 2].stars == 1) {
+            medalImage.GetComponent<Image>().sprite = bronze;
+        } else if(PlayerStats.Levels[SceneManager.GetActiveScene().buildIndex - 2].stars == 2) {
+            medalImage.GetComponent<Image>().sprite = silver;
+        } else {
+            medalImage.GetComponent<Image>().sprite = gold;
+        }
+
+        if (environment.GetComponent<Environment>().moveCounter <= environment.GetComponent<Environment>().moves_gold) {
+            Debug.Log(environment.GetComponent<Environment>().moveCounter);
+            currentMedal.GetComponent<Image>().sprite = gold;
+        } else if (environment.GetComponent<Environment>().moveCounter <= environment.GetComponent<Environment>().moves_silver) {
+            currentMedal.GetComponent<Image>().sprite = silver;
+        } else {
+            currentMedal.GetComponent<Image>().sprite = bronze;
         }
     }
 }
